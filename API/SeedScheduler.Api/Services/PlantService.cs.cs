a96e14c3ad66572373dbdc6b3557d9a5d7a4f3d4
gbrggrn <gustberg107@student.kau.se>
2026-03-20 09:02:11 +0100

    Plant model first draft

--- API/SeedScheduler.Api/Services/PlantService.cs.cs
+++ API/SeedScheduler.Api/Services/PlantService.cs.cs
@@ -2,5 +2,19 @@
 
 public class PlantService
 {
+    // Identifiers
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
 
+    // Growth properties
+    public int SowDepth { get; set; }
+    public int MinGerminationDays { get; set; }
+    public int MaxGerminationDays { get; set; }
+    public int RowSpacing { get; set; }
+    public int PlantSpacing { get; set; }
+    public int MinSowMonth { get; set; }
+    public int MaxSowMonth { get; set; }
+    public int MinHarvestMonth { get; set; }
+    public int MaxHarvestMonth { get; set; }
+    public bool IsPerennial { get; set; }
 }
