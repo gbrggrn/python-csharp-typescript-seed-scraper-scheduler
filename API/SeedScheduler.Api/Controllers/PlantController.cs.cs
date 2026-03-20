71f51ffa2f4402d7d300a1bc93af053c21c4c780
gbrggrn <gustberg107@student.kau.se>
2026-03-20 09:37:52 +0100

    Initial controller + DTO structure

--- API/SeedScheduler.Api/Controllers/PlantController.cs.cs
+++ API/SeedScheduler.Api/Controllers/PlantController.cs.cs
@@ -1,6 +1,48 @@
-﻿namespace SeedScheduler.Api.Controllers;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SeedScheduler.Api.Models;
+using SeedScheduler.Api.Services;
 
-public class PlantController
+namespace SeedScheduler.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class PlantController : Controller
 {
+    private readonly PlantService _plantService;
 
+    public PlantController(PlantService plantService)
+    {
+        _plantService = plantService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ReadAll()
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(PlantCreateDTO plantCreateDTO)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddMultiple(List<PlantCreateDTO> plantCreateDTOs)
+    {
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(PlantUpdateDTO plantUpdateDTO, int id)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete (int id)
+    {
+        return Ok();
+    }
 }
