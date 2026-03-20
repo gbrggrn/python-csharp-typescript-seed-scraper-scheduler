785e413accb308de6e034a3ef4aa29601b0d73cc
gbrggrn <gustberg107@student.kau.se>
2026-03-20 09:05:00 +0100

    Garden model first draft

--- API/SeedScheduler.Api/Models/Garden.cs.cs
+++ API/SeedScheduler.Api/Models/Garden.cs.cs
@@ -2,5 +2,9 @@
 
 public class Garden
 {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
 
+    public float Longitude { get; set; }
+    public float Latitude { get; set; }
 }
