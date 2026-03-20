215845f97991a97e507f12385a8bf4df29d82b66
gbrggrn <gustberg107@student.kau.se>
2026-03-20 10:06:58 +0100

    Register services

--- API/SeedScheduler.Api/Program.cs
+++ API/SeedScheduler.Api/Program.cs
@@ -1,10 +1,15 @@
 using SeedScheduler.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using SeedScheduler.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi(); 
+builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<PlantService>();
+builder.Services.AddScoped<GardenService>();
+builder.Services.AddScoped<ScheduleService>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
