75cf785cb789502a643d468ea076cd3bbebc2e06
gbrggrn <gustberg107@student.kau.se>
2026-03-20 11:35:52 +0100

    GardenService scaffold

--- API/SeedScheduler.Api/Services/GardenService.cs.cs
+++ API/SeedScheduler.Api/Services/GardenService.cs.cs
@@ -1,6 +1,38 @@
-﻿namespace SeedScheduler.Api.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using SeedScheduler.Api.Data;
+using SeedScheduler.Api.Models;
+using SeedScheduler.Api.Shared.DTOs;
+
+namespace SeedScheduler.Api.Services;
 
 public class GardenService
 {
+    private readonly ApplicationDbContext _dbContext;
+    private readonly DbSet<Garden> _gardens;
 
+    public GardenService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _gardens = dbContext.Gardens;
+    }
+
+    public async Task<List<GardenResponseDTO>> ReadAllAsync()
+    {
+        
+    }
+
+    public async Task AddAsync(GardenCreateDTO dto)
+    {
+        
+    }
+
+    public async Task<bool> UpdateAsync(GardenUpdateDTO dto, int id)
+    {
+        
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        
+    }
 }
