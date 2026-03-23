5d8169a3cf8acb2d50de7389797cc60cf0a9bb13
gbrggrn <gustberg107@student.kau.se>
2026-03-23 23:23:34 +0100

    Producer first iteration

--- Scraper/Extract/frobanken-extractor.py
+++ Scraper/Extract/frobanken-extractor.py
@@ -5,4 +5,20 @@ import re
 url_one = "https://xn--frbanken-o4a.se/sitemap.xml?urlset=3"
 url_two = "https://xn--frbanken-o4a.se/sitemap.xml?urlset=4"
 
-print("Environment is wired up")
+reqs_one = requests.get(url_one)
+reqs_two = requests.get(url_two)
+
+soup_one = BeautifulSoup(reqs_one.text, 'xml')
+soup_two = BeautifulSoup(reqs_two.text, 'xml')
+
+with open("frobanken-urls.txt", "w", encoding="utf-8") as file:
+
+    for url in soup_one.find_all('loc'):
+        data = url.text
+        file.write(data)
+        file.write("\n")
+
+    for url in soup_two.find_all('loc'):
+        data = url.text
+        file.write(data)
+        file.write("\n")
