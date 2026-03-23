f482f754d9f98afb5119fc6145df164f7c2f2106
gbrggrn <gustberg107@student.kau.se>
2026-03-23 22:50:46 +0100

    Imports & urls

---
+++ Scraper/Extract/frobanken-extractor.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url_one = "https://xn--frbanken-o4a.se/sitemap.xml?urlset=3"
+url_two = "https://xn--frbanken-o4a.se/sitemap.xml?urlset=4"
+
