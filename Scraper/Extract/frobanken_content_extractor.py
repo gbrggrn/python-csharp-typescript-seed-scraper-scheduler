c3a2515f08a097ccc0e1e563c5a426f36b1327a7
gbrggrn <gustberg107@student.kau.se>
2026-04-01 14:28:50 +0200

    First iteration of html extractor

--- Scraper/Extract/frobanken_content_extractor.py
+++ Scraper/Extract/frobanken_content_extractor.py
@@ -1,11 +1,100 @@
-from bs4 import BeautifulSoup as BF
+from bs4 import BeautifulSoup as BS
 import requests
 
 def retrieve_urls_from_file(filename):
-    vegetable_urls = []
     with open(filename, 'r', encoding = "utf-8") as file:
-        raw_urls = file.read()
-
-        url_list = raw_urls.split("\n")
+        url_list = file.read().splitlines()
 
     return url_list
+
+def extract_content_from_urls(url_list):
+    for url in url_list:
+        if not url:
+            continue
+
+        print(f"Now scraping: {url}")
+
+        response = requests.get(url)
+        soup = BS(response.text, 'html.parser')
+
+        name = soup.select_one('.tws-article-name h1').text
+        print(name)
+
+        description_text = soup.select_one('.tws-article-description-text')
+
+        clean_text = description_text.get_text(separator='\n')
+
+        lines = clean_text.split('\n')
+
+        for line in lines:
+            if "Sådjup:" in line:
+                sow_depth = line.split(":")[1].strip()
+                break
+            if "Grotid:" in line:
+                min_germination_days = line.split(":")[1].split("-")[0].strip()
+                max_germination_days = line.split(":")[1].split("-")[1].split(" ")[0].strip()
+                break
+            if "Höjd:" in line:
+                min_height = line.split(":")[1].split("-")[0].strip()
+                max_height = line.split(":")[1].split("-")[1].split(" ")[0].strip()
+                break
+            if "Radavstånd:" in line:
+                row_spacing = line.split(":")[1].split(" ")[0].strip()
+                break
+            if "Plantavstånd:" in line:
+                plant_spacing = line.split(":")[1].split(" ")[0].strip()
+                break
+            if "Såperiod:" in line:
+                min_month_str = line.split(":")[1].split("-")[0].strip()
+                max_month_str = line.split(":")[1].split("-")[1].strip()
+
+                min_sow_month = month_helper(min_month_str)
+                max_sow_month = month_helper(max_month_str)
+                break
+            if "Skördeperiod:" in line:
+                min_harvest_str = line.split(":")[1].split("-")[0].strip()
+                max_harvest_str = line.split(":")[1].split("-")[0].strip()
+
+                min_harvest = month_helper(min_harvest_str)
+                max_harvest = month_helper(max_harvest_str)
+                break
+
+    DTO = {
+        "name": name,
+        "sow_depth": sow_depth,
+        "min_germination_days": min_germination_days,
+        "max_germination_days": max_germination_days,
+        "min_height": min_height,
+        "max_height": max_height,
+        "row_spacing": row_spacing,
+        "plant_spacing": plant_spacing,
+        "min_sow_month": min_sow_month,
+        "max_sow_month": max_sow_month,
+        "min_harvest": min_harvest,
+        "max_harvest": max_harvest
+    }
+
+    return DTO
+
+def month_helper(month):
+    months_values = {
+        "januari": 1,
+        "februari": 2,
+        "mars": 3,
+        "april": 4,
+        "maj": 5,
+        "juni": 6,
+        "juli": 7,
+        "augusti": 8,
+        "september": 9,
+        "oktober": 10,
+        "november": 11,
+        "december": 12
+    }
+
+    if month in months_values:
+        return months_values.values[month]
+    else:
+        return 0
+            
+
