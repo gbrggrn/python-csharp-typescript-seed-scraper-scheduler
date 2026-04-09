f5c059acae131a83d48421549fd87e718d00e7cb
gbrggrn <gustberg107@student.kau.se>
2026-04-09 09:39:54 +0200

    Split main extraction method -> move DTO creation to transformer

--- Scraper/Extract/frobanken_content_extractor.py
+++ Scraper/Extract/frobanken_content_extractor.py
@@ -9,6 +9,33 @@ def retrieve_urls_from_file(filename):
 
     return url_list
 
+def extract_content(url_list):
+    data = []
+
+    for url in url_list:
+        if not url:
+            continue
+
+        print(f"Now scraping: {url}")
+
+        response = requests.get(url)
+        soup = BS(response.text, 'html.parser')
+
+        name = soup.select_one('.tws-article-name h1').text
+        print(name)
+
+        description_text = soup.select_one('.tws-article-description-text')
+
+        clean_text = description_text.get_text(separator='\n')
+
+        lines = clean_text.split('\n')
+
+        data.append(lines)
+
+        time.sleep(random.uniform(1.5, 3.0))
+    
+    return data
+
 def extract_content_from_urls(url_list):
     extracted_DTOs = []
 
