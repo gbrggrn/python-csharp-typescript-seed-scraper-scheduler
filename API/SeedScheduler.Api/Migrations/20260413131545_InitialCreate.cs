c767e171b015a6fcbe00b5faaf111081a4d39464
gbrggrn <gustberg107@student.kau.se>
2026-04-13 15:19:30 +0200

    Proper SQLite setup + initial migration

---
+++ API/SeedScheduler.Api/Migrations/20260413131545_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeedScheduler.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gardens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Longitude = table.Column<float>(type: "REAL", nullable: false),
+                    Latitude = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gardens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Plants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    SowDepth = table.Column<int>(type: "INTEGER", nullable: false),
+                    MinGerminationDays = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaxGerminationDays = table.Column<int>(type: "INTEGER", nullable: false),
+                    RowSpacing = table.Column<int>(type: "INTEGER", nullable: false),
+                    PlantSpacing = table.Column<int>(type: "INTEGER", nullable: false),
+                    MinSowMonth = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaxSowMonth = table.Column<int>(type: "INTEGER", nullable: false),
+                    MinHarvestMonth = table.Column<int>(type: "INTEGER", nullable: false),
+                    MaxHarvestMonth = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsPerennial = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Gardens");
+
+            migrationBuilder.DropTable(
+                name: "Plants");
+
+            migrationBuilder.DropTable(
+                name: "Schedules");
+        }
+    }
+}
