b784d4aabf8c9cced7a03538f4c47052616b4b5f
gbrggrn <gustberg107@student.kau.se>
2026-04-21 21:30:38 +0200

    Fixed the transformer trying to fetch name from list of objects instead of the object which lines are being iterated

--- Scraper/Transform/frobanken_transformer.py
+++ Scraper/Transform/frobanken_transformer.py
@@ -54,7 +54,7 @@ def extract_DTOs_from_data(data):
             print(f"[transformer] PLANT CONTENT:\nSådjup:{sow_depth}\nGrotid:{min_germination_days}-{max_germination_days}\nHöjd:{min_height}-{max_height}\nRadavstånd:{row_spacing}\nPlantavstånd:{plant_spacing}\nSåperiod: {min_sow_month}-{max_sow_month}\nSkördeperiod: {min_sow_month}-{max_sow_month}")
 
             DTO = {
-                "name": data["name"],
+                "name": entry["name"],
                 "sow_depth": sow_depth,
                 "min_germination_days": min_germination_days,
                 "max_germination_days": max_germination_days,
