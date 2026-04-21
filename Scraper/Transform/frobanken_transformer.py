e285587ab9073f4d35d2eb3a5fa1c98034f8a8ed
gbrggrn <gustberg107@student.kau.se>
2026-04-21 21:25:45 +0200

    Added debug statement and removed outer loop of extractor function

--- Scraper/Transform/frobanken_transformer.py
+++ Scraper/Transform/frobanken_transformer.py
@@ -6,7 +6,8 @@ def transform(data):
 def extract_DTOs_from_data(data):
     extracted_DTOs = []
     
-    for plant_dict in data:
+    for line in data["lines"]:
+
         sow_depth = ""
         min_germination_days = ""
         max_germination_days = ""
@@ -19,38 +20,39 @@ def extract_DTOs_from_data(data):
         min_harvest = ""
         max_harvest = ""
 
-        for line in plant_dict:
-            if "Sådjup:" in line:
-                sow_depth = line.split(":")[1].strip()
-            if "Grotid:" in line:
-                min_germination_days = line.split(":")[1].split("–")[0].strip()
-                max_germination_days = line.split(":")[1].split("–")[1].split(" ")[0].strip()
-            if "Höjd:" in line:
-                min_height_split = line.split(":")[1]
-                if "–" in min_height_split:
-                    min_height = min_height_split.split("–")[0].strip()
-                    max_height = min_height_split.split("–")[1].strip()
-                else:
-                    min_height = min_height_split.split(" ")[0].strip()
-            if "Radavstånd:" in line:
-                row_spacing = line.split(":")[1].split(" ")[0].strip()
-            if "Plantavstånd:" in line:
-                plant_spacing = line.split(":")[1].split(" ")[0].strip()
-            if "Såperiod:" in line:
-                min_month_str = line.split(":")[1].split("–")[0].strip()
-                max_month_str = line.split(":")[1].split("–")[1].strip()
+        if "Sådjup:" in line:
+            sow_depth = line.split(":")[1].strip()
+        if "Grotid:" in line:
+            min_germination_days = line.split(":")[1].split("–")[0].strip()
+            max_germination_days = line.split(":")[1].split("–")[1].split(" ")[0].strip()
+        if "Höjd:" in line:
+            min_height_split = line.split(":")[1]
+            if "–" in min_height_split:
+                min_height = min_height_split.split("–")[0].strip()
+                max_height = min_height_split.split("–")[1].strip()
+            else:
+                min_height = min_height_split.split(" ")[0].strip()
+        if "Radavstånd:" in line:
+            row_spacing = line.split(":")[1].split(" ")[0].strip()
+        if "Plantavstånd:" in line:
+            plant_spacing = line.split(":")[1].split(" ")[0].strip()
+        if "Såperiod:" in line:
+            min_month_str = line.split(":")[1].split("–")[0].strip()
+            max_month_str = line.split(":")[1].split("–")[1].strip()
 
-                min_sow_month = month_helper(min_month_str)
-                max_sow_month = month_helper(max_month_str)
-            if "Skördeperiod:" in line:
-                min_harvest_str = line.split(":")[1].split("–")[0].strip()
-                max_harvest_str = line.split(":")[1].split("–")[1].strip()
+            min_sow_month = month_helper(min_month_str)
+            max_sow_month = month_helper(max_month_str)
+        if "Skördeperiod:" in line:
+            min_harvest_str = line.split(":")[1].split("–")[0].strip()
+            max_harvest_str = line.split(":")[1].split("–")[1].strip()
 
-                min_harvest = month_helper(min_harvest_str)
-                max_harvest = month_helper(max_harvest_str)
+            min_harvest = month_helper(min_harvest_str)
+            max_harvest = month_helper(max_harvest_str)
+
+        print(f"[transformer] PLANT CONTENT:\nSådjup:{sow_depth}\nGrotid:{min_germination_days}-{max_germination_days}\nHöjd:{min_height}-{max_height}\nRadavstånd:{row_spacing}\nPlantavstånd:{plant_spacing}\nSåperiod: {min_sow_month}-{max_sow_month}\nSkördeperiod: {min_sow_month}-{max_sow_month}")
 
         DTO = {
-            "name": plant_dict["name"],
+            "name": data["name"],
             "sow_depth": sow_depth,
             "min_germination_days": min_germination_days,
             "max_germination_days": max_germination_days,
