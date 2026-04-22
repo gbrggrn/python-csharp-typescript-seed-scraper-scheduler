48a03fdb1122f59c5bd06d703a3056cd75e9fc0e
gbrggrn <gustberg107@student.kau.se>
2026-04-22 09:54:48 +0200

    Commented out everything but the extraction from orchestrator

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,10 +1,10 @@
-from Extract.frobanken_url_extractor import filter_and_save_clean_urls
-from Extract.frobanken_url_extractor import harvest_raw_urls
+# from Extract.frobanken_url_extractor import filter_and_save_clean_urls
+#from Extract.frobanken_url_extractor import harvest_raw_urls
 from Extract.frobanken_content_extractor import extract
-from Transform.frobanken_transformer import transform
-from Load.frobanken_loader import load
-from pathlib import Path
-
+#from Transform.frobanken_transformer import transform
+#from Load.frobanken_loader import load
+#from pathlib import Path
+"""
 # Extract URLs
 print("==========\nRetrieving URLs...\n==========")
 savefile = Path("C:\\Users\\gusta\\source\\repos\\Random\\python-csharp-typescript-seed-scraper-scheduler\\Scraper\\frobanken-raw-urls.txt")
@@ -18,10 +18,10 @@ else:
     print("[scraper] RAW-URLs already extracted. Filtering...")
     filter_and_save_clean_urls()
     print("[scraper] Filtering done. Continuing...")
-
+"""
 print("==========\nExtracting data...\n==========")
 data = extract()
-
+"""
 # Transform
 print("==========\nPackaging data...\n===========")
 DTOs = transform(data)
@@ -31,3 +31,4 @@ print("==========\nLoading data...\n===========")
 # load(DTOs)
 
 print("==========\nETL SCRIPTING FINISHED...\n==========")
+"""
