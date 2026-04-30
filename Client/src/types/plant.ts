b399034d802d9aee229f115f1ba54fe726fadec1
gbrggrn <gustberg107@student.kau.se>
2026-04-30 22:59:25 +0200

    Defined Plant interface

---
+++ Client/src/types/plant.ts
@@ -0,0 +1,14 @@
+export interface Plant {
+    name: string,
+    sowDepth: number,
+    minGerminationDays: number,
+    maxGerminationDays: number,
+    minHeight: number,
+    maxHeight: number,
+    rowSpacing: number,
+    plantSpacing: number,
+    minSowMonth: number,
+    maxSowMonth: number,
+    minHarvestMonth: number,
+    maxHarvestMonth: number
+}
