6b20cf46487c161d744ca5505bf43db11b0abd6b
gbrggrn <gustberg107@student.kau.se>
2026-05-06 11:18:35 +0200

    Defined basic IDE-like rendering structure

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,5 +1,26 @@
 import { useState } from 'react'
+import Header from './components/header'
+import Sidebar from './components/sidebar'
+import GanttChart from './components/stage'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  return (
+    <div className="ide-wrapper">
+      <Header />
+      <div className="workspace">
+        <main className="stage">
+          <GanttChart selectedPlants={selectedPlants} />
+        </main>
+        <aside className="sidebar-right">
+          <PlantExplorer
+            plants={plants}
+            onToggle={togglePlantSelection}
+            />
+        </aside>
+      </div>
+    </div>
+  );
+}
