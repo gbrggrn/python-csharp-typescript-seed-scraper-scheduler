cb6aa9a98d60f74945a82139dad58c06e8b724fd
gbrggrn <gustberg107@student.kau.se>
2026-05-07 10:54:25 +0200

    Added the call to plantService fetchPlants

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import Header from './components/header/Header'
 import Sidebar from './components/sidebar/Sidebar'
 import Stage from './components/stage/Stage'
 import { type Plant } from './types/plant'
+import { fetchPlants } from './services/plantService'
 import './App.css'
 
 function App() {
-  const [plants, setPlants] = useState<Plant[]>([])
+  const [plants, setPlants] = useState<Plant[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const data = await fetchPlants();
+        setPlants(data);
+      } catch (err) {
+        setError("Failed to load plants.");
+        console.error(err);
+      }
+    };
+
+    loadData();
+  }, []);
 
   return (
     <div className="ide-wrapper">
