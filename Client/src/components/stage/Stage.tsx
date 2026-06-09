d56b04c20c42ab831f7cca836258faf66bc53c1e
gbrggrn <gustberg107@student.kau.se>
2026-06-09 11:27:44 +0200

    Defined header row (no CSS yet)

--- Client/src/components/stage/Stage.tsx
+++ Client/src/components/stage/Stage.tsx
@@ -10,5 +10,19 @@ export default function Stage({ selectedIds, allPlants }: StageProps) {
 
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Okt', 'Nov', 'Dec']
 
-    
+    return (
+        <main className="stage">
+            <h2>Såddschema</h2>
+            
+            <div className="gantt-chart">
+                {/* HEADER */}
+                <div className="chart-row header-row">
+                    <div className="cell-name">Grönsak</div>
+                    { months.map(m => <div key={m} className="cell-month">{m}</div>)}
+                </div>
+
+                
+            </div>
+        </main>
+    )
 }
