9f6b9cd6658a5539d499c091c8e562c18099339b
gbrggrn <gustberg107@student.kau.se>
2026-06-11 12:21:08 +0200

    Added new migration

---
+++ API/SeedScheduler.Api/Migrations/20260611102014_UpdateSchemaForDescriptionUrlUid.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeedScheduler.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSchemaForDescriptionUrlUid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPerennial",
+                table: "Plants");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Uid",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Plants",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "Uid",
+                table: "Plants");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Plants");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPerennial",
+                table: "Plants",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
