1a91050c2d8619df33147ef7eca4dfab8d2dd7d5
gbrggrn <gustberg107@student.kau.se>
2026-06-18 14:09:40 +0200

    Made to match model

--- API/SeedScheduler.Api/Shared/DTOs/GardenUpdateDTO.cs
+++ API/SeedScheduler.Api/Shared/DTOs/GardenUpdateDTO.cs
@@ -2,11 +2,12 @@
 
 public class GardenUpdateDTO
 {
-    // Identifiers
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
 
-    // Location
     public float Longitude { get; set; }
     public float Latitude { get; set; }
+
+    public int AverageFirstFrostDay { get; set; } // (1-365)
+    public int AverageLastFrostDay { get; set; } // (1-365)
 }
