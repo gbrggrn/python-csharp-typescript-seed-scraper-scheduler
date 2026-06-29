ae40c0b55df2d7abb059a112eba022476d9b7a1a
gbrggrn <gustberg107@student.kau.se>
2026-06-29 15:17:33 +0200

    Added new migration for garden frost dates

---
+++ API/SeedScheduler.Api/Migrations/20260629131619_GardensFrostMigration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeedScheduler.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class GardensFrostMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AverageFirstFrostDay",
+                table: "Gardens",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AverageLastFrostDay",
+                table: "Gardens",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageFirstFrostDay",
+                table: "Gardens");
+
+            migrationBuilder.DropColumn(
+                name: "AverageLastFrostDay",
+                table: "Gardens");
+        }
+    }
+}
