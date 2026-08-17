1fad99e191208f9e8b102992db5362ba3307bf12
gbrggrn <gustberg107@student.kau.se>
2026-08-17 09:17:29 +0200

    Added baseAdress for locally hosted LLM on different machine

--- API/SeedScheduler.Api/Services/LLMClient.cs.cs
+++ API/SeedScheduler.Api/Services/LLMClient.cs.cs
@@ -3,9 +3,15 @@ namespace SeedScheduler.Api.Services;
 public class LLMClient
 {
     private readonly HttpClient _httpClient;
+    private readonly string baseAddress = "http://192.168.39.179:11434";
 
     public LLMClient()
     {
-        _httpClient = new HttpClient();
+        _httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri(baseAddress)
+        };
     }
+
+    
 }
