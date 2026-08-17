68f1fd252e4dd4ec10b2899ca572ea6bddf44b94
gbrggrn <gustberg107@student.kau.se>
2026-08-17 09:08:31 +0200

    Class decl + init httpClient

--- API/SeedScheduler.Api/Services/LLMClient.cs.cs
+++ API/SeedScheduler.Api/Services/LLMClient.cs.cs
@@ -1,1 +1,11 @@
 namespace SeedScheduler.Api.Services;
+
+public class LLMClient
+{
+    private readonly HttpClient _httpClient;
+
+    public LLMClient()
+    {
+        _httpClient = new HttpClient();
+    }
+}
